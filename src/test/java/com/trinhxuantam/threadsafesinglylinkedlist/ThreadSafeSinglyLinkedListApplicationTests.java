a009b180072536abcf6c67b86ddbbb6a7f2626d0
Trinh Xuan Tam <mail@trinhxuantam.com>
2024-04-26 14:46:25 +0200

    initial commit

---
+++ src/test/java/com/trinhxuantam/threadsafesinglylinkedlist/ThreadSafeSinglyLinkedListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trinhxuantam.threadsafesinglylinkedlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThreadSafeSinglyLinkedListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
